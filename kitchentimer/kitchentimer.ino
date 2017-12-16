bae33b64409a02bc5a670cfe3a43b83ae6df35ea
utylee <utylee@gmail.com>
2017-12-16 16:16:24 +0900

    editing kitchentimer

--- kitchentimer/kitchentimer.ino
+++ kitchentimer/kitchentimer.ino
@@ -3,11 +3,14 @@ int btn_start = 0;		// 타이머 스타트 버튼
 int btn_inc = 0;		// 타이머 + 버튼
 int btn_dec = 0;		// 타이머 - 버튼
 int init_min = 5;		// 기본 설정 분
+int minute = 0;			// 분
+int second = 0;			// 초
 
 
 
 void setup()
 {
+	//처음엔 5분 숫자를 표시
 }
 
 
