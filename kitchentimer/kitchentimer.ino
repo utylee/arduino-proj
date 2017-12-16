df65d741daf1c8d7ad53a6a4a8ba9e6cca474a79
utylee <utylee@gmail.com>
2017-12-16 15:45:40 +0900

    added

---
+++ kitchentimer/kitchentimer.ino
@@ -0,0 +1,23 @@
+
+int btn_start = 0;		// 타이머 스타트 버튼
+int btn_inc = 0;		// 타이머 + 버튼
+int btn_dec = 0;		// 타이머 - 버튼
+int init_min = 5;		// 기본 설정 분
+
+
+
+void setup()
+{
+}
+
+
+void loop()
+{
+
+}
+
+
+void start_timer()
+{
+
+}
